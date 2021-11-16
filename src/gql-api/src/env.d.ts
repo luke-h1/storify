042195a772555926838533babed8ad5d63e440be
luke-h1 <luke.howsam@yahoo.com>
2021-11-16 20:45:48 +0000

    dockerize db

--- src/gql-api/src/env.d.ts
+++ src/gql-api/src/env.d.ts
@@ -2,5 +2,9 @@ declare namespace NodeJS {
   interface ProcessEnv {
     CORS_ORIGIN: string;
     SESSION_SECRET: string;
+    DATABASE_URL: string;
+    REDIS_URL: string;
+    PORT: string;
+    FRONTEND_URL: string;
   }
 }
