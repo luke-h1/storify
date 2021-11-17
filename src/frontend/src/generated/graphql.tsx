bd6a890722ea0ec5efdec98589eed28f2b12dfea
luke-h1 <luke.howsam@yahoo.com>
2021-11-17 22:59:55 +0000

    generate init graphql schema for fe

---
+++ src/frontend/src/generated/graphql.tsx
@@ -0,0 +1,125 @@
+import gql from 'graphql-tag';
+import * as Urql from 'urql';
+
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** The `Upload` scalar type represents a file upload. */
+  Upload: any;
+};
+
+export type FieldError = {
+  __typename?: 'FieldError';
+  field: Scalars['String'];
+  message: Scalars['String'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  changePassword: UserResponse;
+  forgotPassword: Scalars['Boolean'];
+  login: UserResponse;
+  logout: Scalars['Boolean'];
+  register: UserResponse;
+};
+
+export type MutationChangePasswordArgs = {
+  newPassword: Scalars['String'];
+  token: Scalars['String'];
+};
+
+export type MutationForgotPasswordArgs = {
+  email: Scalars['String'];
+};
+
+export type MutationLoginArgs = {
+  email: Scalars['String'];
+  password: Scalars['String'];
+};
+
+export type MutationRegisterArgs = {
+  image?: InputMaybe<Scalars['Upload']>;
+  options: UserRegisterInput;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  me?: Maybe<User>;
+};
+
+export type User = {
+  __typename?: 'User';
+  createdAt: Scalars['String'];
+  email: Scalars['String'];
+  firstName: Scalars['String'];
+  fullName: Scalars['String'];
+  id: Scalars['Int'];
+  image: Scalars['String'];
+  isAdmin: Scalars['String'];
+  lastName: Scalars['String'];
+  updatedAt: Scalars['String'];
+};
+
+export type UserRegisterInput = {
+  bio: Scalars['String'];
+  email: Scalars['String'];
+  firstName: Scalars['String'];
+  lastName: Scalars['String'];
+  password: Scalars['String'];
+};
+
+export type UserResponse = {
+  __typename?: 'UserResponse';
+  errors?: Maybe<Array<FieldError>>;
+  user?: Maybe<User>;
+};
+
+export type UserFragmentFragment = {
+  __typename?: 'User';
+  id: number;
+  email: string;
+};
+
+export type MeQueryVariables = Exact<{ [key: string]: never }>;
+
+export type MeQuery = {
+  __typename?: 'Query';
+  me?: { __typename?: 'User'; id: number; email: string } | null | undefined;
+};
+
+export const UserFragmentFragmentDoc = gql`
+  fragment UserFragment on User {
+    id
+    email
+  }
+`;
+export const MeDocument = gql`
+  query Me {
+    me {
+      ...UserFragment
+    }
+  }
+  ${UserFragmentFragmentDoc}
+`;
+
+export function useMeQuery(
+  options: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'query'> = {},
+) {
+  return Urql.useQuery<MeQuery>({ query: MeDocument, ...options });
+}
