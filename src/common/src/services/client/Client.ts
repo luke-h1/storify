47a94021ec27be74cac1244230e64770cf996679
luke-h1 <luke.howsam@yahoo.com>
2021-11-22 22:51:44 +0000

    services

--- src/common/src/services/client/Client.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const storifyApi = axios.create({
-  baseURL: 'http://localhost:8000',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-})
-export default storifyApi;
