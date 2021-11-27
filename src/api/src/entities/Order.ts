a344644569779f834da73d7c3c8ec514445710b6
luke-h1 <luke.howsam@yahoo.com>
2021-11-27 15:36:35 +0000

    updated to order entity

--- src/api/src/entities/Order.ts
+++ src/api/src/entities/Order.ts
@@ -27,6 +27,30 @@ export class Order extends BaseEntity {
   @Column()
   email: string;
 
+  @Field(() => String)
+  @Column()
+  TransactionId: string;
+
+  @Field(() => String)
+  @Column()
+  address: string;
+
+  @Field(() => String)
+  @Column()
+  city: string;
+
+  @Field(() => String)
+  @Column()
+  country: string;
+
+  @Field(() => String)
+  @Column()
+  zip: string;
+
+  @Field(() => String)
+  @Column()
+  complete: string;
+
   @Field(() => [OrderItem])
   @OneToMany(() => OrderItem, orderItem => orderItem.order)
   orderItems: OrderItem[];
