3abdd16bb32293b21f4bd0a62b1a483407de5e04
luke-h1 <luke.howsam@yahoo.com>
2021-11-27 12:20:00 +0000

    add cloudinary

--- src/api/src/types/env.d.ts
+++ src/api/src/types/env.d.ts
@@ -6,9 +6,6 @@ declare namespace NodeJS {
     REDIS_URL: string;
     PORT: string;
     FRONTEND_URL: string;
-    AWS_ACCESS_KEY_ID: string;
-    AWS_SECRET_ACCESS_KEY: string;
-    AWS_BUCKET_NAME: string;
-    AWS_BUCKET_REGION: string;
+    CLOUDINARY_SECRET: string;
   }
 }
--- src/frontend/src/types/env.d.ts
+++ src/frontend/src/types/env.d.ts
@@ -1,5 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_STORIFY_API_URL: string;
+    NEXT_PUBLIC_CLOUDINARY_KEY: string;
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: string;
   }
 }
