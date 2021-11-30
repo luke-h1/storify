57cf6bccfbe237ec06173c83df9d89e17969da4f
luke-h1 <luke.howsam@yahoo.com>
2021-11-30 10:48:50 +0000

    work on product card

---
+++ src/frontend/src/components/ProductCard.tsx
@@ -0,0 +1,96 @@
+import {
+  Box,
+  Heading,
+  Link,
+  Image,
+  Text,
+  Divider,
+  HStack,
+  Tag,
+  Wrap,
+  WrapItem,
+  SpaceProps,
+  useColorModeValue,
+  Container,
+  VStack,
+} from '@chakra-ui/react';
+import React from 'react';
+
+interface IBlogTags {
+  tags: string[];
+  marginTop?: SpaceProps['marginTop'];
+}
+
+const BlogTags: React.FC<IBlogTags> = ({ tags, marginTop }) => {
+  return (
+    <HStack spacing={2} marginTop={marginTop}>
+      {tags.map(tag => {
+        return (
+          <Tag size="md" variant="solid" colorScheme="orange" key={tag}>
+            {tag}
+          </Tag>
+        );
+      })}
+    </HStack>
+  );
+};
+
+interface BlogAuthorProps {
+  date: Date;
+  name: string;
+}
+
+export const BlogAuthor: React.FC<BlogAuthorProps> = ({ name, date }) => {
+  return (
+    <HStack marginTop="2" spacing="2" display="flex" alignItems="center">
+      <Image
+        borderRadius="full"
+        boxSize="40px"
+        src="https://100k-faces.glitch.me/random-image"
+        alt={`Avatar of ${name}`}
+      />
+      <Text fontWeight="medium">{name}</Text>
+      <Text>—</Text>
+      <Text>{date.toLocaleDateString()}</Text>
+    </HStack>
+  );
+};
+
+const ProductCard = () => {
+  return (
+    <WrapItem width={{ base: '100%', sm: '45%', md: '45%', lg: '30%' }}>
+      <Box w="100%">
+        <Box borderRadius="lg" overflow="hidden">
+          <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+            <Image
+              transform="scale(1.0)"
+              src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80"
+              alt="some text"
+              objectFit="contain"
+              width="100%"
+              transition="0.3s ease-in-out"
+              _hover={{
+                transform: 'scale(1.05)',
+              }}
+            />
+          </Link>
+        </Box>
+        <BlogTags tags={['Engineering', 'Product']} marginTop="3" />
+        <Heading fontSize="xl" marginTop="2">
+          <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+            Some blog title
+          </Link>
+        </Heading>
+        <Text as="p" fontSize="md" marginTop="2">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </Text>
+        <BlogAuthor name="John Doe" date={new Date('2021-04-06T19:01:27Z')} />
+      </Box>
+    </WrapItem>
+  );
+};
+
+export default ProductCard;
--- src/frontend/src/pages/index.tsx
+++ src/frontend/src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { Container, Heading, Divider, Wrap } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import { withUrqlClient } from 'next-urql';
+import React from 'react';
+import ProductCard from '../components/ProductCard';
+import { createurqlClient } from '../utils/createUrqlClient';
 
 const Home: NextPage = () => {
-  return <p>hello</p>;
+  return (
+    <Container maxW="7xl" p="12">
+      <Heading as="h2" marginTop="5">
+        Latest products
+      </Heading>
+      <Divider marginTop="5" />
+      <Wrap spacing="30px" marginTop="5">
+        test
+      </Wrap>
+    </Container>
+  );
 };
 
-export default Home;
+export default withUrqlClient(createurqlClient, { ssr: true })(Home);
