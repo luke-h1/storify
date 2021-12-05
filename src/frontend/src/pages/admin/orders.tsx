ae22ba88a28cafc7c8dbbe560652f371193d2b25
luke-h1 <luke.howsam@yahoo.com>
2021-12-05 16:16:23 +0000

    remove unused imports

--- src/frontend/src/pages/admin/orders.tsx
+++ src/frontend/src/pages/admin/orders.tsx
@@ -1,11 +1,5 @@
-import { ButtonGroup, IconButton } from '@chakra-ui/button';
-import { useColorModeValue } from '@chakra-ui/color-mode';
-import { Flex } from '@chakra-ui/layout';
-import { Table, Thead, Tr, Th, Tbody, Td } from '@chakra-ui/table';
 import { withUrqlClient } from 'next-urql';
 import React from 'react';
-import { AiFillEdit } from 'react-icons/ai';
-import { BsBoxArrowUpRight, BsFillTrashFill } from 'react-icons/bs';
 import { useOrdersQuery } from '../../generated/graphql';
 import { createurqlClient } from '../../utils/createUrqlClient';
 import { isServer } from '../../utils/isServer';
