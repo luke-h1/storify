e4eb9e0fab61718a8805e4c0c962a204a97531ec
luke-h1 <luke.howsam@yahoo.com>
2021-12-05 16:07:38 +0000

    feat/seed-database (calculate total of all orderItems on given order)

--- src/frontend/src/generated/graphql.tsx
+++ src/frontend/src/generated/graphql.tsx
@@ -1,10 +1,17 @@
 import gql from 'graphql-tag';
 import * as Urql from 'urql';
+
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
@@ -46,43 +53,35 @@ export type Mutation = {
   updateProduct?: Maybe<Product>;
 };
 
-
 export type MutationChangePasswordArgs = {
   newPassword: Scalars['String'];
   token: Scalars['String'];
 };
 
-
 export type MutationCreateOrderArgs = {
   input: OrderCreateInput;
 };
-
 
 export type MutationCreateProductArgs = {
   input: ProductCreateInput;
 };
 
-
 export type MutationDeleteProductArgs = {
   id: Scalars['Int'];
 };
 
-
 export type MutationForgotPasswordArgs = {
   email: Scalars['String'];
 };
-
 
 export type MutationLoginArgs = {
   email: Scalars['String'];
   password: Scalars['String'];
 };
 
-
 export type MutationRegisterArgs = {
   options: UserRegisterInput;
 };
-
 
 export type MutationUpdateProductArgs = {
   id: Scalars['Int'];
@@ -122,6 +121,7 @@ export type OrderItem = {
   createdAt: Scalars['String'];
   id: Scalars['Int'];
   order: Order;
+  orderId: Scalars['Int'];
   price: Scalars['Int'];
   productTitle: Scalars['String'];
   qty: Scalars['Int'];
@@ -162,7 +162,6 @@ export type Query = {
   products: Array<Product>;
 };
 
-
 export type QueryProductArgs = {
   id: Scalars['Int'];
 };
@@ -192,264 +191,439 @@ export type UserResponse = {
   user?: Maybe<User>;
 };
 
-export type ErrorFragment = { __typename?: 'FieldError', field: string, message: string };
+export type ErrorFragment = {
+  __typename?: 'FieldError';
+  field: string;
+  message: string;
+};
 
-export type UserFragmentFragment = { __typename?: 'User', id: number, email: string, firstName: string, isAdmin: boolean };
+export type UserFragmentFragment = {
+  __typename?: 'User';
+  id: number;
+  email: string;
+  firstName: string;
+  isAdmin: boolean;
+};
 
-export type UserResponseFragmentFragment = { __typename?: 'UserResponse', errors?: Array<{ __typename?: 'FieldError', field: string, message: string }> | null | undefined, user?: { __typename?: 'User', id: number, email: string, firstName: string, isAdmin: boolean } | null | undefined };
+export type UserResponseFragmentFragment = {
+  __typename?: 'UserResponse';
+  errors?:
+    | Array<{ __typename?: 'FieldError'; field: string; message: string }>
+    | null
+    | undefined;
+  user?:
+    | {
+        __typename?: 'User';
+        id: number;
+        email: string;
+        firstName: string;
+        isAdmin: boolean;
+      }
+    | null
+    | undefined;
+};
 
 export type CreateProductMutationVariables = Exact<{
   input: ProductCreateInput;
 }>;
 
+export type CreateProductMutation = {
+  __typename?: 'Mutation';
+  createProduct: {
+    __typename?: 'Product';
+    id: number;
+    image: string;
+    name: string;
+    price: number;
+    brand: string;
+    categories: Array<string>;
+    descriptionSnippet: string;
+  };
+};
 
-export type CreateProductMutation = { __typename?: 'Mutation', createProduct: { __typename?: 'Product', id: number, image: string, name: string, price: number, brand: string, categories: Array<string>, descriptionSnippet: string } };
+export type CreateSignatureMutationVariables = Exact<{ [key: string]: never }>;
 
-export type CreateSignatureMutationVariables = Exact<{ [key: string]: never; }>;
-
-
-export type CreateSignatureMutation = { __typename?: 'Mutation', createImageSignature: { __typename?: 'ImageSignature', signature: string, timestamp: number } };
+export type CreateSignatureMutation = {
+  __typename?: 'Mutation';
+  createImageSignature: {
+    __typename?: 'ImageSignature';
+    signature: string;
+    timestamp: number;
+  };
+};
 
 export type DeleteProductMutationVariables = Exact<{
   id: Scalars['Int'];
 }>;
 
-
-export type DeleteProductMutation = { __typename?: 'Mutation', deleteProduct: boolean };
+export type DeleteProductMutation = {
+  __typename?: 'Mutation';
+  deleteProduct: boolean;
+};
 
 export type LoginMutationVariables = Exact<{
   email: Scalars['String'];
   password: Scalars['String'];
 }>;
 
+export type LoginMutation = {
+  __typename?: 'Mutation';
+  login: {
+    __typename?: 'UserResponse';
+    errors?:
+      | Array<{ __typename?: 'FieldError'; field: string; message: string }>
+      | null
+      | undefined;
+    user?:
+      | {
+          __typename?: 'User';
+          id: number;
+          email: string;
+          firstName: string;
+          isAdmin: boolean;
+        }
+      | null
+      | undefined;
+  };
+};
 
-export type LoginMutation = { __typename?: 'Mutation', login: { __typename?: 'UserResponse', errors?: Array<{ __typename?: 'FieldError', field: string, message: string }> | null | undefined, user?: { __typename?: 'User', id: number, email: string, firstName: string, isAdmin: boolean } | null | undefined } };
+export type LogoutMutationVariables = Exact<{ [key: string]: never }>;
 
-export type LogoutMutationVariables = Exact<{ [key: string]: never; }>;
-
-
-export type LogoutMutation = { __typename?: 'Mutation', logout: boolean };
+export type LogoutMutation = { __typename?: 'Mutation'; logout: boolean };
 
 export type RegisterMutationVariables = Exact<{
   options: UserRegisterInput;
 }>;
 
-
-export type RegisterMutation = { __typename?: 'Mutation', register: { __typename?: 'UserResponse', errors?: Array<{ __typename?: 'FieldError', field: string, message: string }> | null | undefined, user?: { __typename?: 'User', id: number, email: string, firstName: string, isAdmin: boolean } | null | undefined } };
+export type RegisterMutation = {
+  __typename?: 'Mutation';
+  register: {
+    __typename?: 'UserResponse';
+    errors?:
+      | Array<{ __typename?: 'FieldError'; field: string; message: string }>
+      | null
+      | undefined;
+    user?:
+      | {
+          __typename?: 'User';
+          id: number;
+          email: string;
+          firstName: string;
+          isAdmin: boolean;
+        }
+      | null
+      | undefined;
+  };
+};
 
 export type UpdateProductMutationVariables = Exact<{
   input: ProductCreateInput;
   id: Scalars['Int'];
 }>;
 
+export type UpdateProductMutation = {
+  __typename?: 'Mutation';
+  updateProduct?:
+    | {
+        __typename?: 'Product';
+        id: number;
+        image: string;
+        brand: string;
+        categories: Array<string>;
+        description: string;
+        price: number;
+        publicId: string;
+        creator: { __typename?: 'User'; fullName: string };
+      }
+    | null
+    | undefined;
+};
 
-export type UpdateProductMutation = { __typename?: 'Mutation', updateProduct?: { __typename?: 'Product', id: number, image: string, brand: string, categories: Array<string>, description: string, price: number, publicId: string, creator: { __typename?: 'User', fullName: string } } | null | undefined };
+export type MeQueryVariables = Exact<{ [key: string]: never }>;
 
-export type MeQueryVariables = Exact<{ [key: string]: never; }>;
+export type MeQuery = {
+  __typename?: 'Query';
+  me?:
+    | {
+        __typename?: 'User';
+        id: number;
+        email: string;
+        firstName: string;
+        isAdmin: boolean;
+      }
+    | null
+    | undefined;
+};
 
+export type OrdersQueryVariables = Exact<{ [key: string]: never }>;
 
-export type MeQuery = { __typename?: 'Query', me?: { __typename?: 'User', id: number, email: string, firstName: string, isAdmin: boolean } | null | undefined };
-
-export type OrdersQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-export type OrdersQuery = { __typename?: 'Query', orders: Array<{ __typename?: 'Order', id: number, firstName: string, lastName: string, orderItems: Array<{ __typename?: 'OrderItem', id: number, price: number, productTitle: string, qty: number }> }> };
+export type OrdersQuery = {
+  __typename?: 'Query';
+  orders: Array<{
+    __typename?: 'Order';
+    id: number;
+    firstName: string;
+    lastName: string;
+    orderItems: Array<{
+      __typename?: 'OrderItem';
+      id: number;
+      price: number;
+      productTitle: string;
+      qty: number;
+    }>;
+  }>;
+};
 
 export type ProductQueryVariables = Exact<{
   id: Scalars['Int'];
 }>;
 
+export type ProductQuery = {
+  __typename?: 'Query';
+  product?:
+    | {
+        __typename?: 'Product';
+        id: number;
+        brand: string;
+        categories: Array<string>;
+        description: string;
+        image: string;
+        price: number;
+        name: string;
+        creator: { __typename?: 'User'; id: number };
+      }
+    | null
+    | undefined;
+};
 
-export type ProductQuery = { __typename?: 'Query', product?: { __typename?: 'Product', id: number, brand: string, categories: Array<string>, description: string, image: string, price: number, name: string, creator: { __typename?: 'User', id: number } } | null | undefined };
+export type ProductsQueryVariables = Exact<{ [key: string]: never }>;
 
-export type ProductsQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-export type ProductsQuery = { __typename?: 'Query', products: Array<{ __typename?: 'Product', id: number, brand: string, categories: Array<string>, descriptionSnippet: string, image: string, name: string, price: number, creator: { __typename?: 'User', fullName: string } }> };
+export type ProductsQuery = {
+  __typename?: 'Query';
+  products: Array<{
+    __typename?: 'Product';
+    id: number;
+    brand: string;
+    categories: Array<string>;
+    descriptionSnippet: string;
+    image: string;
+    name: string;
+    price: number;
+    creator: { __typename?: 'User'; fullName: string };
+  }>;
+};
 
 export const ErrorFragmentDoc = gql`
-    fragment Error on FieldError {
-  field
-  message
-}
-    `;
+  fragment Error on FieldError {
+    field
+    message
+  }
+`;
 export const UserFragmentFragmentDoc = gql`
-    fragment UserFragment on User {
-  id
-  email
-  firstName
-  isAdmin
-}
-    `;
-export const UserResponseFragmentFragmentDoc = gql`
-    fragment UserResponseFragment on UserResponse {
-  errors {
-    ...Error
-  }
-  user {
-    ...UserFragment
-  }
-}
-    ${ErrorFragmentDoc}
-${UserFragmentFragmentDoc}`;
-export const CreateProductDocument = gql`
-    mutation CreateProduct($input: ProductCreateInput!) {
-  createProduct(input: $input) {
+  fragment UserFragment on User {
     id
-    image
-    name
-    price
-    brand
-    categories
-    descriptionSnippet
+    email
+    firstName
+    isAdmin
   }
-}
-    `;
+`;
+export const UserResponseFragmentFragmentDoc = gql`
+  fragment UserResponseFragment on UserResponse {
+    errors {
+      ...Error
+    }
+    user {
+      ...UserFragment
+    }
+  }
+  ${ErrorFragmentDoc}
+  ${UserFragmentFragmentDoc}
+`;
+export const CreateProductDocument = gql`
+  mutation CreateProduct($input: ProductCreateInput!) {
+    createProduct(input: $input) {
+      id
+      image
+      name
+      price
+      brand
+      categories
+      descriptionSnippet
+    }
+  }
+`;
 
 export function useCreateProductMutation() {
-  return Urql.useMutation<CreateProductMutation, CreateProductMutationVariables>(CreateProductDocument);
-};
-export const CreateSignatureDocument = gql`
-    mutation CreateSignature {
-  createImageSignature {
-    signature
-    timestamp
-  }
+  return Urql.useMutation<
+    CreateProductMutation,
+    CreateProductMutationVariables
+  >(CreateProductDocument);
 }
-    `;
+export const CreateSignatureDocument = gql`
+  mutation CreateSignature {
+    createImageSignature {
+      signature
+      timestamp
+    }
+  }
+`;
 
 export function useCreateSignatureMutation() {
-  return Urql.useMutation<CreateSignatureMutation, CreateSignatureMutationVariables>(CreateSignatureDocument);
-};
-export const DeleteProductDocument = gql`
-    mutation DeleteProduct($id: Int!) {
-  deleteProduct(id: $id)
+  return Urql.useMutation<
+    CreateSignatureMutation,
+    CreateSignatureMutationVariables
+  >(CreateSignatureDocument);
 }
-    `;
+export const DeleteProductDocument = gql`
+  mutation DeleteProduct($id: Int!) {
+    deleteProduct(id: $id)
+  }
+`;
 
 export function useDeleteProductMutation() {
-  return Urql.useMutation<DeleteProductMutation, DeleteProductMutationVariables>(DeleteProductDocument);
-};
-export const LoginDocument = gql`
-    mutation Login($email: String!, $password: String!) {
-  login(email: $email, password: $password) {
-    ...UserResponseFragment
-  }
+  return Urql.useMutation<
+    DeleteProductMutation,
+    DeleteProductMutationVariables
+  >(DeleteProductDocument);
 }
-    ${UserResponseFragmentFragmentDoc}`;
+export const LoginDocument = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      ...UserResponseFragment
+    }
+  }
+  ${UserResponseFragmentFragmentDoc}
+`;
 
 export function useLoginMutation() {
   return Urql.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument);
-};
-export const LogoutDocument = gql`
-    mutation Logout {
-  logout
 }
-    `;
+export const LogoutDocument = gql`
+  mutation Logout {
+    logout
+  }
+`;
 
 export function useLogoutMutation() {
-  return Urql.useMutation<LogoutMutation, LogoutMutationVariables>(LogoutDocument);
-};
-export const RegisterDocument = gql`
-    mutation Register($options: UserRegisterInput!) {
-  register(options: $options) {
-    ...UserResponseFragment
-  }
+  return Urql.useMutation<LogoutMutation, LogoutMutationVariables>(
+    LogoutDocument,
+  );
 }
-    ${UserResponseFragmentFragmentDoc}`;
+export const RegisterDocument = gql`
+  mutation Register($options: UserRegisterInput!) {
+    register(options: $options) {
+      ...UserResponseFragment
+    }
+  }
+  ${UserResponseFragmentFragmentDoc}
+`;
 
 export function useRegisterMutation() {
-  return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument);
-};
+  return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(
+    RegisterDocument,
+  );
+}
 export const UpdateProductDocument = gql`
-    mutation UpdateProduct($input: ProductCreateInput!, $id: Int!) {
-  updateProduct(input: $input, id: $id) {
-    id
-    image
-    brand
-    categories
-    description
-    price
-    publicId
-    creator {
-      fullName
+  mutation UpdateProduct($input: ProductCreateInput!, $id: Int!) {
+    updateProduct(input: $input, id: $id) {
+      id
+      image
+      brand
+      categories
+      description
+      price
+      publicId
+      creator {
+        fullName
+      }
     }
   }
-}
-    `;
+`;
 
 export function useUpdateProductMutation() {
-  return Urql.useMutation<UpdateProductMutation, UpdateProductMutationVariables>(UpdateProductDocument);
-};
+  return Urql.useMutation<
+    UpdateProductMutation,
+    UpdateProductMutationVariables
+  >(UpdateProductDocument);
+}
 export const MeDocument = gql`
-    query Me {
-  me {
-    ...UserFragment
+  query Me {
+    me {
+      ...UserFragment
+    }
   }
-}
-    ${UserFragmentFragmentDoc}`;
+  ${UserFragmentFragmentDoc}
+`;
 
-export function useMeQuery(options: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'query'> = {}) {
+export function useMeQuery(
+  options: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'query'> = {},
+) {
   return Urql.useQuery<MeQuery>({ query: MeDocument, ...options });
-};
+}
 export const OrdersDocument = gql`
-    query Orders {
-  orders {
-    id
-    firstName
-    lastName
-    orderItems {
+  query Orders {
+    orders {
       id
-      price
-      productTitle
-      qty
+      firstName
+      lastName
+      orderItems {
+        id
+        price
+        productTitle
+        qty
+      }
     }
   }
-}
-    `;
+`;
 
-export function useOrdersQuery(options: Omit<Urql.UseQueryArgs<OrdersQueryVariables>, 'query'> = {}) {
+export function useOrdersQuery(
+  options: Omit<Urql.UseQueryArgs<OrdersQueryVariables>, 'query'> = {},
+) {
   return Urql.useQuery<OrdersQuery>({ query: OrdersDocument, ...options });
-};
+}
 export const ProductDocument = gql`
-    query Product($id: Int!) {
-  product(id: $id) {
-    id
-    brand
-    categories
-    brand
-    description
-    image
-    price
-    name
-    creator {
+  query Product($id: Int!) {
+    product(id: $id) {
       id
+      brand
+      categories
+      brand
+      description
+      image
+      price
+      name
+      creator {
+        id
+      }
     }
   }
-}
-    `;
+`;
 
-export function useProductQuery(options: Omit<Urql.UseQueryArgs<ProductQueryVariables>, 'query'> = {}) {
+export function useProductQuery(
+  options: Omit<Urql.UseQueryArgs<ProductQueryVariables>, 'query'> = {},
+) {
   return Urql.useQuery<ProductQuery>({ query: ProductDocument, ...options });
-};
+}
 export const ProductsDocument = gql`
-    query Products {
-  products {
-    id
-    brand
-    categories
-    brand
-    descriptionSnippet
-    image
-    name
-    price
-    creator {
-      fullName
+  query Products {
+    products {
+      id
+      brand
+      categories
+      brand
+      descriptionSnippet
+      image
+      name
+      price
+      creator {
+        fullName
+      }
     }
   }
-}
-    `;
+`;
 
-export function useProductsQuery(options: Omit<Urql.UseQueryArgs<ProductsQueryVariables>, 'query'> = {}) {
+export function useProductsQuery(
+  options: Omit<Urql.UseQueryArgs<ProductsQueryVariables>, 'query'> = {},
+) {
   return Urql.useQuery<ProductsQuery>({ query: ProductsDocument, ...options });
-};
+}
--- src/frontend/src/pages/admin/orders.tsx
+++ src/frontend/src/pages/admin/orders.tsx
@@ -1,16 +1,11 @@
-import { Box, Flex, Heading } from '@chakra-ui/layout';
-import {
-  Table,
-  Thead,
-  Tr,
-  Th,
-  Tbody,
-  Td,
-  TableCaption,
-  Tfoot,
-} from '@chakra-ui/react';
+import { ButtonGroup, IconButton } from '@chakra-ui/button';
+import { useColorModeValue } from '@chakra-ui/color-mode';
+import { Flex } from '@chakra-ui/layout';
+import { Table, Thead, Tr, Th, Tbody, Td } from '@chakra-ui/table';
 import { withUrqlClient } from 'next-urql';
 import React from 'react';
+import { AiFillEdit } from 'react-icons/ai';
+import { BsBoxArrowUpRight, BsFillTrashFill } from 'react-icons/bs';
 import { useOrdersQuery } from '../../generated/graphql';
 import { createurqlClient } from '../../utils/createUrqlClient';
 import { isServer } from '../../utils/isServer';
@@ -18,53 +13,145 @@ import { isServer } from '../../utils/isServer';
 const OrdersPage = () => {
   const [{ data, fetching }] = useOrdersQuery({ pause: isServer() });
 
-  if (!data && fetching) {
-    return <p>fetching</p>;
-  }
+  const header = ['name', 'created', 'actions'];
+  const fakeData = [
+    { name: 'Daggy', created: '7 days ago' },
+    { name: 'Anubra', created: '23 hours ago' },
+    { name: 'Josef', created: 'A few seconds ago' },
+    { name: 'Sage', created: 'A few hours ago' },
+  ];
   return (
-    <>
-      <Box mb={10}>
-        <Heading as="h1">Orders</Heading>
-      </Box>
-      {data?.orders &&
-        data?.orders.map(order => (
-          <Table variant="simple" key={order.id}>
-            <TableCaption>Imperial to metric conversion factors</TableCaption>
-
-            <Thead>
-              <Tr>
-                <Th>To convert</Th>
-                <Th>into</Th>
-                <Th isNumeric>multiply by</Th>
+    <Flex w="full" p={50} alignItems="center" justifyContent="center">
+      <Table
+        w="full"
+        display={{
+          base: 'block',
+          md: 'table',
+        }}
+        sx={{
+          '@media print': {
+            display: 'table',
+          },
+        }}
+      >
+        <Thead
+          display={{
+            base: 'none',
+            md: 'table-header-group',
+          }}
+          sx={{
+            '@media print': {
+              display: 'table-header-group',
+            },
+          }}
+        >
+          <Tr>
+            <Th>#</Th>
+            <Th>Product Title</Th>
+            <Th>Price</Th>
+            <Th>Qty</Th>
+          </Tr>
+        </Thead>
+        <Tbody
+          display={{
+            base: 'block',
+            lg: 'table-row-group',
+          }}
+          sx={{
+            '@media print': {
+              display: 'table-row-group',
+            },
+          }}
+        >
+          {/* {fakeData.map((token, tid) => {
+            return (
+              <Tr
+                key={tid}
+                display={{
+                  base: 'grid',
+                  md: 'table-row',
+                }}
+                sx={{
+                  '@media print': {
+                    display: 'table-row',
+                  },
+                  gridTemplateColumns: 'minmax(0px, 35%) minmax(0px, 65%)',
+                  gridGap: '10px',
+                }}
+              >
+                {Object.keys(token).map(x => {
+                  return (
+                    <React.Fragment key={`${tid}${x}`}>
+                      <Td
+                        display={{
+                          base: 'table-cell',
+                          md: 'none',
+                        }}
+                        sx={{
+                          '@media print': {
+                            display: 'none',
+                          },
+                          textTransform: 'uppercase',
+                          color: '#000',
+                          fontSize: 'xs',
+                          fontWeight: 'bold',
+                          letterSpacing: 'wider',
+                          fontFamily: 'heading',
+                        }}
+                      >
+                        {x}
+                      </Td>
+                      <Td color="#000" fontSize="md" fontWeight="hairline">
+                        {token[x]}
+                      </Td>
+                    </React.Fragment>
+                  );
+                })}
+                <Td
+                  display={{
+                    base: 'table-cell',
+                    md: 'none',
+                  }}
+                  sx={{
+                    '@media print': {
+                      display: 'none',
+                    },
+                    textTransform: 'uppercase',
+                    color: '#000',
+                    fontSize: 'sm',
+                    fontWeight: 'bold',
+                    letterSpacing: 'wider',
+                    fontFamily: 'heading',
+                  }}
+                >
+                  Actions
+                </Td>
+                <Td>
+                  <ButtonGroup variant="solid" size="sm" spacing={3}>
+                    <IconButton
+                      aria-label="test"
+                      colorScheme="blue"
+                      icon={<BsBoxArrowUpRight />}
+                    />
+                    <IconButton
+                      colorScheme="green"
+                      icon={<AiFillEdit />}
+                      aria-label="test"
+                    />
+                    <IconButton
+                      aria-label="test"
+                      colorScheme="red"
+                      variant="outline"
+                      icon={<BsFillTrashFill />}
+                    />
+                  </ButtonGroup>
+                </Td>
               </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>inches</Td>
-                <Td>millimetres (mm)</Td>
-                <Td isNumeric>25.4</Td>
-              </Tr>
-              <Tr>
-                <Td>feet</Td>
-                <Td>centimetres (cm)</Td>
-                <Td isNumeric>30.48</Td>
-              </Tr>
-              <Tr>
-                <Td>yards</Td>
-                <Td>metres (m)</Td>
-                <Td isNumeric>0.91444</Td>
-              </Tr>
-            </Tbody>
-            <Tfoot>
-              <Tr>
-                <Th>To convert</Th>
-                <Th>into</Th>
-                <Th isNumeric>multiply by</Th>
-              </Tr>
-            </Tfoot>
-          </Table>
-        ))}
-    </>
+            );
+          })} */}
+        </Tbody>
+      </Table>
+    </Flex>
   );
 };
 export default withUrqlClient(createurqlClient, { ssr: true })(OrdersPage);
