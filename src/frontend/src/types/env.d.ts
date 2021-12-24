b4beb855318c3db0ba226da7ce35a670d8d20cab
luke-h1 <luke.howsam@yahoo.com>
2021-12-24 17:48:32 +0000

    feat(api): change API port

--- src/frontend/src/types/env.d.ts
+++ src/frontend/src/types/env.d.ts
@@ -3,6 +3,5 @@ declare namespace NodeJS {
     NEXT_PUBLIC_STORIFY_API_URL: string;
     NEXT_PUBLIC_CLOUDINARY_KEY: string;
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: string;
-    NEXT_PUBLIC_STRIPE_KEY: string;
   }
 }
