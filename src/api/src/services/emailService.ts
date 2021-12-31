1d50598e035132ed9d28765b4024cf35914f035d
luke-h1 <luke.howsam@yahoo.com>
2021-12-31 22:41:55 +0000

    cleanup email service

--- src/api/src/services/emailService.ts
+++ src/api/src/services/emailService.ts
@@ -5,26 +5,32 @@ import formData from 'form-data';
 import Mailgun from 'mailgun.js';
 
 const mailgun = new Mailgun(formData);
+
 const client = mailgun.client({
   username: 'api',
   key: process.env.MAILGUN_API_KEY,
 });
 
-const messageData = {
-  from: 'Excited User <me@samples.mailgun.org>',
-  to: 'foo@example.com, bar@example.com',
-  subject: 'Hello',
-  text: 'Testing some Mailgun awesomeness!',
+const emailService = {
+  sendEmail: async (
+    from: string,
+    to: string,
+    subject: string,
+    text: string,
+  ) => {
+    try {
+      const res = await client.messages.create(process.env.EMAIL_DOMAIN_NAME, {
+        from,
+        to,
+        subject,
+        text,
+      });
+
+      console.log('sent email', res);
+    } catch (e) {
+      console.error(e);
+    }
+  },
 };
 
-client.messages
-  .create(process.env.EMAIL_DOMAIN_NAME, messageData)
-  .then(res => {
-    console.log(res);
-  })
-  .catch(err => {
-    console.error(err);
-  });
-
-const emailService = {};
 export default emailService;
