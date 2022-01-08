918a02d31d761e8106ac4061e0ae996cb2f034ef
luke-h1 <luke.howsam@yahoo.com>
2022-01-08 11:45:05 +0000

    fix admin order page

--- src/frontend/src/pages/admin/orders.tsx
+++ src/frontend/src/pages/admin/orders.tsx
@@ -21,41 +21,115 @@ const Orders: NextPage = () => {
   return (
     <AdminRoute>
       <Page title="Orders (admin) | Storify">
-        <h1 style={{ marginBottom: '2rem' }}>Manage orders on the service</h1>
-        <table style={{ marginTop: '0.5rem' }} className="table">
-          <thead>
-            <td>Order ID</td>
-            <td>Order Status</td>
-            <td>Total</td>
-            <td>User ID</td>
-            <td>QTY</td>
-            <td>Product ID</td>
-            <td>Product Name</td>
-            <td>Product image</td>
-            <td>Product Price</td>
-          </thead>
-          {data?.orders &&
-            data?.orders.map(order => (
-              <tbody key={order.id}>
-                <tr key={order.id}>
-                  <td>{order.id}</td>
-                  <td>{order.status}</td>
-                  <td>{order.total}</td>
-                  <td>{order.creatorId}</td>
-                  {order?.orderDetails &&
-                    order?.orderDetails.map(od => (
-                      <>
-                        <td>{od.quantity}</td>
-                        <td>{od.product.id}</td>
-                        <td>{od.product.name}</td>
-                        <td>{od.product.image}</td>
-                        <td>{od.product.price}</td>
-                      </>
+        <div className="bg-white p-8 rounded-md w-full max-w-800">
+          <div className="flex items-center justify-bbetween pb-6">
+            <div>
+              <h2 className="text-gray-600 font-semibold">
+                Manage orders on the service
+              </h2>
+            </div>
+          </div>
+          <div>
+            <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+              <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                <table className="min-w-full leading-normal">
+                  <thead>
+                    <tr>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Order ID
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Order status
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Total
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        User ID
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        QTY
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Product ID
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Product Name
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Product image
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        Product Price
+                      </th>
+                    </tr>
+                  </thead>
+                  {data?.orders &&
+                    data?.orders.map(order => (
+                      <tbody key={order.id}>
+                        <tr>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 whitespace-no-wrap">
+                              {order.id}
+                            </p>
+                          </td>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 whitespace-no-wrap">
+                              {order.status}
+                            </p>
+                          </td>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 whitespace-no-wrap">
+                              {order.total}
+                            </p>
+                          </td>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 whitespace-no-wrap">
+                              {order.creatorId}
+                            </p>
+                          </td>
+                          {order.orderDetails &&
+                            order.orderDetails.map(od => (
+                              <>
+                                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                  <p className="text-gray-900 whitespace-no-wrap">
+                                    {od.quantity}
+                                  </p>
+                                </td>
+                                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                  <p className="text-gray-900 whitespace-no-wrap">
+                                    {od.product.id}
+                                  </p>
+                                </td>
+                                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                  <p className="text-gray-900 whitespace-no-wrap">
+                                    {od.product.name}
+                                  </p>
+                                </td>
+                                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                  <p className="text-gray-900 whitespace-no-wrap">
+                                    <img
+                                      src={od.product.image}
+                                      style={{ maxWidth: '100px' }}
+                                      alt={od.product.name}
+                                    />
+                                  </p>
+                                </td>
+                                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                  <p className="text-gray-900 whitespace-no-wrap">
+                                    £{od.product.price.toFixed(2)}
+                                  </p>
+                                </td>
+                              </>
+                            ))}
+                        </tr>
+                      </tbody>
                     ))}
-                </tr>
-              </tbody>
-            ))}
-        </table>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </Page>
     </AdminRoute>
   );
