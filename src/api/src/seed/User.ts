2f273d138eeec6c5c76b11655bf482af860f95da
luke-h1 <luke.howsam@yahoo.com>
2022-01-09 13:43:35 +0000

    feat(api): add user seed

---
+++ src/api/src/seed/User.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+/* eslint-disable no-await-in-loop */
+import 'dotenv-safe/config';
+import bcrypt from 'bcryptjs';
+import { getManager } from 'typeorm';
+import createConn from '../db/createConn';
+import { User } from '../entities/User';
+
+(async () => {
+  try {
+    await createConn();
+
+    const userRepository = await getManager().getRepository(User);
+    const hashedPassword = await bcrypt.hash('storify12345', 12);
+
+    for (let i = 0; i < 60; i += 1) {
+      const result = await userRepository.save({
+        firstName: `test ${i}`,
+        lastName: `test ${i}`,
+        email: `test${i}@test.com`,
+        password: hashedPassword,
+      });
+      console.log(`User created: ${result.id}`);
+    }
+    const adminUser = await userRepository.save({
+      firstname: 'admin',
+      lastName: 'admin',
+      email: 'admin@test.com',
+      password: hashedPassword,
+      isAdmin: true,
+    });
+    console.log(`Admin user created: ${adminUser.email}`);
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
