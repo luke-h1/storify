9d0327157e14d74f14eaec277d098851e7a944a3
luke-h1 <luke.howsam@yahoo.com>
2022-01-09 20:03:40 +0000

    fix unit tests

--- src/api/jest.config.js
+++ src/api/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/src/__tests__/*.(test).*'],
+  testMatch: ['<rootDir>/src/**/__tests__/*.(test).*'],
 };
