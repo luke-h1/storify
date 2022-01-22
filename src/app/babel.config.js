e518ca54d5de7f3fdb88d2b63b73adf0bd5e06bb
luke-h1 <luke.howsam@yahoo.com>
2022-01-22 16:23:50 +0000

    remove app

--- src/app/babel.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-module.exports = function (api) {
-  api.cache(true);
-  const plugins = ['module:react-native-dotenv'];
-  return {
-    presets: ['babel-preset-expo'],
-    plugins,
-  };
-};
